1809a55e4a16eee8bfe7f53647fe5266c6b8f6b4
Samuel <59849928+SJeffries19@users.noreply.github.com>
2023-03-01 20:27:13 -0500

    Initial branch commit

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <MenuBarLimited />
+
+    <h1>Login</h1>
+    <div class="field">
+        <input type="text" placeholder="Enter Username" id="username" name="username" required />
+        <input type="password" placeholder="Enter Password" id="password" name="password" required />
+        <button type="submit">Log in</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import MenuBarLimited from './MenuBarLimited.vue';
+
+export default {
+  name: 'LoginPage',
+
+  components: {
+    MenuBarLimited
+  },
+
+  data() {
+    return {
+      activeItem: 'Home',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+.field input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+
+.field button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  background: skyblue;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/MenuBarLimited.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+  
+      <div class="menu-container">
+        <div class="title">NucScholar</div>
+          <div class="menu-item" :class="{'active': activeItem === 'Home'}" @click="activeItem = 'Home'">Home</div>
+      </div>
+    </div>
+  </template>
+
+  <script>
+export default {
+  name: 'MenuBarLimited',
+  data() {
+    return {
+      activeItem: 'Home',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+/* Styles for the horizontal menu */
+.menu-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #0000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+.menu-item {
+  margin-left: 10px;
+  font-size: 18px;
+  cursor: pointer;
+}
+
+/* Style for the active menu item */
+.active {
+  color: #4CAF50;
+  font-weight: bold;
+}
+
+/* Style for the title */
+.title {
+  font-size: 24px;
+  font-weight: bold;
+  margin-right: 20px;
+}
+</style>
