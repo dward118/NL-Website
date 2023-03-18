e56ddd9785778a1034d8cdbc3885fcfe0f253f1c
Samuel <59849928+SJeffries19@users.noreply.github.com>
2023-03-18 01:13:42 -0400

    Further refactoring. Should make future development far less confusing.

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,24 +1,20 @@
 <template>
-  <div>
-    <div class="menu-container">
-      <div class="menu-item"><router-link to="/">
-          <div style="font-size: 30px; color: #fac62e; font-weight: bold;" onmouseover="this.style.color='aliceblue'" onmouseout="this.style.color='#fac62e'">NucScholar</div>
-        </router-link></div>
-      <div class="menu-item"><router-link to="/question">Ask a Question</router-link></div>
-      <div class="menu-item"><router-link to="/semantic">Search by Meaning</router-link></div>
-      <div class="menu-item"><router-link to="/contact-us">Contact Us</router-link></div>
-      <div class="menu-item"><router-link to="/login">Login</router-link></div>
-    </div>
-    <footer>
-      <div class="footer-content">
-        <p class="footerStyle">
-          NucScholar is a product of Lawrence Berkeley National Laboaratory &trade;
-      and The Air Force Institute of Technology &trade;
-        </p>
-      </div>
-    </footer>
-
+  <div class="menu-item">
+    <a><router-link to="/" style="font-size: 30px; color: #fac62e; font-weight: bold;"
+        onmouseover="this.style.color='aliceblue'" onmouseout="this.style.color='#fac62e'">NucScholar</router-link></a>
+    <a><router-link to="/question">Ask a Question</router-link></a>
+    <a><router-link to="/semantic">Search by Meaning</router-link></a>
+    <a><router-link to="/contact-us">Contact Us</router-link></a>
+    <a><router-link to="/login">Login</router-link></a>
   </div>
+  <footer>
+    <div class="footer-content">
+      <p class="footerStyle">
+        NucScholar is a product of Lawrence Berkeley National Laboaratory &trade;
+        and The Air Force Institute of Technology &trade;
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -29,36 +25,27 @@ export default {
     
 <style scoped>
 /* Styles for the horizontal menu */
-.menu-container {
+.menu-item {
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   background-color: #00313c;
-  /*justify-content: space-between;*/
-  display: flex;
-  padding: 10px;
   overflow: hidden;
-}
-
-.menu-item {
-  font-size: 18px;
-  padding: 14px 40px;
-  cursor: pointer;
-  color: aliceblue;
 }
 
 .menu-item a {
   float: left;
   color: aliceblue;
-  padding: 0px 30px;
+  text-align: center;
+  padding: 14px 16px;
   font-size: 18px;
   text-decoration: none;
+  margin-right: 5px;
 }
 
 .menu-item a:hover {
   color: #fac62e;
-  text-decoration: none;
 }
 
 footer {
@@ -85,5 +72,4 @@ footer-content {
 .footerStyle {
   text-align: center;
 }
-
 </style>
--- src/components/MenuBarLimited.vue
+++ src/components/MenuBarLimited.vue
@@ -1,20 +1,16 @@
 <template>
-  <div>
-    <div class="menu-container">
-      <div class="menu-item"><router-link to="/">
-          <div style="font-size: 30px; color: #fac62e; font-weight: bold;" onmouseover="this.style.color='aliceblue'"
-            onmouseout="this.style.color='#fac62e'">NucScholar</div>
-        </router-link></div>
-    </div>
-    <footer>
-      <div class="footer-content">
-        <p class="footerStyle">
-          NucScholar is a product of Lawrence Berkeley National Laboaratory &trade;
-          and The Air Force Institute of Technology &trade;
-        </p>
-      </div>
-    </footer>
+  <div class="menu-item">
+    <a><router-link to="/" style="font-size: 30px; color: #fac62e; font-weight: bold;"
+        onmouseover="this.style.color='aliceblue'" onmouseout="this.style.color='#fac62e'">NucScholar</router-link></a>
   </div>
+  <footer>
+    <div class="footer-content">
+      <p class="footerStyle">
+        NucScholar is a product of Lawrence Berkeley National Laboaratory &trade;
+        and The Air Force Institute of Technology &trade;
+      </p>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -25,36 +21,27 @@ export default {
 
 <style scoped>
 /* Styles for the horizontal menu */
-.menu-container {
+.menu-item {
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   background-color: #00313c;
-  /*justify-content: space-between;*/
-  display: flex;
-  padding: 10px;
   overflow: hidden;
-}
-
-.menu-item {
-  font-size: 18px;
-  padding: 14px 40px;
-  cursor: pointer;
-  color: aliceblue;
 }
 
 .menu-item a {
   float: left;
   color: aliceblue;
-  padding: 0px 30px;
+  text-align: center;
+  padding: 14px 16px;
   font-size: 18px;
   text-decoration: none;
+  margin-right: 5px;
 }
 
 .menu-item a:hover {
   color: #fac62e;
-  text-decoration: none;
 }
 
 footer {
