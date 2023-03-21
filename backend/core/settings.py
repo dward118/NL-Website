3659e6e282b5f8bc0ab22c1c2edbebeab5246c22
Redhawk18 <redhawk76767676@gmail.com>
2023-03-21 12:43:07 -0400

    fixed typo

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -34,7 +34,7 @@ REST_FRAMEWORK = {
         
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.isAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
