3e6e3e3a54838bde940e30836d532ae77d385e69
Redhawk18 <Redhawk76767676@gmail.com>
2023-03-31 22:50:23 -0400

    minor name change

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,9 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
-from .forms import CustomUserCreationForm
+from .forms import UserCreationForm
 
 class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
+    form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
