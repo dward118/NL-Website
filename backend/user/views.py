9694cd50bf94835de875abb2db86c50bf7ff58c7
Redhawk18 <Redhawk76767676@gmail.com>
2023-04-15 23:08:51 -0400

    removed unuse imports

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,5 @@
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializer import UserSerializer, RegisterSerializer, CustomTokenObtainPairSerializer
