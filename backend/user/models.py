f5821c079ba8d7223e3e0d3550b69a8cd5883162
Redhawk18 <Redhawk76767676@gmail.com>
2023-04-15 23:01:40 -0400

    changed experience datatype

--- backend/user/models.py
+++ backend/user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     institution = models.TextField(editable=True, null=False)
-    experience = models.IntegerField(editable=True, null=False)
+    experience = models.PositiveSmallIntegerField(editable=True, null=False)
     approved = models.BooleanField(default=False)
 
     REQUIRED_FIELDS = ["first_name", "last_name", "email", "institution", "experience"]
